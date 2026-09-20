99979265f50ff43f876577d0c6787eb46d46adb2
jeffreyumanzor <jumanzor@proton.me>
2026-09-21 00:12:30 +0200

    testing maps

---
+++ test_1.py
@@ -0,0 +1,14 @@
+from src.parser import Parser
+from src.router import Scheduler
+from src.models import Drone
+
+graph, nb_drones = Parser(
+    "maps/maps/challenger/01_the_impossible_dream.txt").parse()
+if graph.start_hub:
+    drones = [Drone(i, graph.start_hub) for i in range(1, nb_drones + 1)]
+
+plans = Scheduler(graph, drones).build_plans()
+for plan in plans:
+    print(plan.drone.name, "llega en turno", plan.arrival_turn)
+    for move in plan.moves:
+        print("  turno", move.turn, "->", move.label)
